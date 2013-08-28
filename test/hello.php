3c399a9ef5dc2c0ff841a8efb5a0a45c4b6f6aae
davergina <dave.ergina@hotmail.com>
2013-08-28 16:15:11 +0800

    First

--- test/hello.php
+++ test/hello.php
@@ -1,10 +1,14 @@
-<?php
-/**
- * Created by JetBrains PhpStorm.
- * User: dave
- * Date: 8/28/13
- * Time: 4:05 PM
- * To change this template use File | Settings | File Templates.
- */
-//hello
-?>
+<html>
+    <head>
+
+    </head>
+
+    <body>
+    <?php
+        $a = "test";
+
+        echo $a;
+    ?>
+
+    </body>
+</html>
